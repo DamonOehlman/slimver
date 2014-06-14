14282be25ea3af3c49ffc1e00b6701df1da1a538
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-14 21:32:25 +1000

    Add range test for specific version

--- test/range.js
+++ test/range.js
@@ -140,3 +140,13 @@ test('range "" = [0.0.0, 65535.65535.65535]', function(t) {
   t.equal(slim.stringify(range[0]), '0.0.0');
   t.equal(slim.stringify(range[1]), '65535.65535.65535');
 });
+
+test('range 1.2.3 = [1.2.3, 1.2.3]', function(t) {
+  var range;
+
+  t.plan(3);
+  range = slim.range('1.2.3');
+  t.ok(Array.isArray(range));
+  t.equal(slim.stringify(range[0]), '1.2.3');
+  t.equal(slim.stringify(range[1]), '1.2.3');
+});
