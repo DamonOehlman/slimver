fb70d34ef610c74594fc1080df0d45ab81ce5dbb
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-14 20:37:05 +1000

    More comprehensive range tests

--- test/range.js
+++ test/range.js
@@ -30,3 +30,113 @@ test('range ^1 = [1.0.0, 1.65535.65535]', function(t) {
   t.equal(slim.stringify(range[0]), '1.0.0');
   t.equal(slim.stringify(range[1]), '1.65535.65535');
 });
+
+test('range ^1.0 = [1.0.0, 1.65535.65535]', function(t) {
+  var range;
+
+  t.plan(3);
+  range = slim.range('^1.0');
+  t.ok(Array.isArray(range));
+  t.equal(slim.stringify(range[0]), '1.0.0');
+  t.equal(slim.stringify(range[1]), '1.65535.65535');
+});
+
+test('range ^0.2.3 = [0.2.3, 0.2.65535]', function(t) {
+  var range;
+
+  t.plan(3);
+  range = slim.range('^0.2.3');
+  t.ok(Array.isArray(range));
+  t.equal(slim.stringify(range[0]), '0.2.3');
+  t.equal(slim.stringify(range[1]), '0.2.65535');
+});
+
+test('range ~0.2.3 = [0.2.3, 0.2.65535]', function(t) {
+  var range;
+
+  t.plan(3);
+  range = slim.range('~0.2.3');
+  t.ok(Array.isArray(range));
+  t.equal(slim.stringify(range[0]), '0.2.3');
+  t.equal(slim.stringify(range[1]), '0.2.65535');
+});
+
+test('range ^0.2 = [0.2.0, 0.2.65535]', function(t) {
+  var range;
+
+  t.plan(3);
+  range = slim.range('^0.2');
+  t.ok(Array.isArray(range));
+  t.equal(slim.stringify(range[0]), '0.2.0');
+  t.equal(slim.stringify(range[1]), '0.2.65535');
+});
+
+test('range ^0.0.5 = [0.0.5, 0.0.5]', function(t) {
+  var range;
+
+  t.plan(3);
+  range = slim.range('^0.0.5');
+  t.ok(Array.isArray(range));
+  t.equal(slim.stringify(range[0]), '0.0.5');
+  t.equal(slim.stringify(range[1]), '0.0.5');
+});
+
+test('range 1.x.x = [1.0.0, 1.65535.65535]', function(t) {
+  var range;
+
+  t.plan(3);
+  range = slim.range('1.x.x');
+  t.ok(Array.isArray(range));
+  t.equal(slim.stringify(range[0]), '1.0.0');
+  t.equal(slim.stringify(range[1]), '1.65535.65535');
+});
+
+test('range 1.2.x = [1.2.0, 1.65535.65535]', function(t) {
+  var range;
+
+  t.plan(3);
+  range = slim.range('1.2.x');
+  t.ok(Array.isArray(range));
+  t.equal(slim.stringify(range[0]), '1.2.0');
+  t.equal(slim.stringify(range[1]), '1.65535.65535');
+});
+
+test('range 0.3.x = [0.3.0, 0.3.65535]', function(t) {
+  var range;
+
+  t.plan(3);
+  range = slim.range('0.3.x');
+  t.ok(Array.isArray(range));
+  t.equal(slim.stringify(range[0]), '0.3.0');
+  t.equal(slim.stringify(range[1]), '0.3.65535');
+});
+
+test('range * = [0.0.0, 65535.65535.65535]', function(t) {
+  var range;
+
+  t.plan(3);
+  range = slim.range('*');
+  t.ok(Array.isArray(range));
+  t.equal(slim.stringify(range[0]), '0.0.0');
+  t.equal(slim.stringify(range[1]), '65535.65535.65535');
+});
+
+test('range "any" = [0.0.0, 65535.65535.65535]', function(t) {
+  var range;
+
+  t.plan(3);
+  range = slim.range('any');
+  t.ok(Array.isArray(range));
+  t.equal(slim.stringify(range[0]), '0.0.0');
+  t.equal(slim.stringify(range[1]), '65535.65535.65535');
+});
+
+test('range "" = [0.0.0, 65535.65535.65535]', function(t) {
+  var range;
+
+  t.plan(3);
+  range = slim.range('');
+  t.ok(Array.isArray(range));
+  t.equal(slim.stringify(range[0]), '0.0.0');
+  t.equal(slim.stringify(range[1]), '65535.65535.65535');
+});
